ac2ea30d5f7f55a31573169fbac4166886efb84e
Aryaman Kolhe <chasmiccoder@gmail.com>
2022-01-30 11:47:59 +0530

    Got EPR Pair

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Quantum-Maze
 A Quantum Maze game that can be solved only with successful quantum key distribution.
+
+### Problem Statement
+Bob is stuck in a dark maze, but is supplied with its map. As the maze is treacherous, he cannot  
+stick to one wall and escape. He also has cloud access to an intermediate interface, through which he can communicate with a quantum computer and Alice.
+All he needs to know to escape is his current position (x,y). Alice found her way to the control room, which 
+
+
+There is are only 2 safe paths, which are the shortest Manhattan paths between Bob and the exit (0,0)
+
+
---
+++ quantum_backend.py
@@ -0,0 +1,44 @@
+"""
+
+NOTE:
+Very useful video -
+https://www.youtube.com/watch?v=u_K9jPBrOwA
+
+"""
+
+import os
+from apikey import token
+
+from qiskit import execute
+from qiskit.circuit import QuantumRegister, ClassicalRegister, QuantumCircuit
+
+from quantuminspire.credentials import enable_account, get_authentication
+from quantuminspire.qiskit import QI
+
+enable_account(token)
+
+QI_URL = os.getenv('API_URL', 'https://api.quantum-inspire.com/')
+
+project_name = 'Qiskit-entangle'
+authentication = get_authentication()
+# QI.set_authentication(authentication, QI_URL, project_name=project_name)
+QI.set_authentication()
+qi_backend = QI.get_backend('QX single-node simulator')
+
+q = QuantumRegister(2)
+b = ClassicalRegister(2)
+circuit = QuantumCircuit(q, b)
+
+circuit.h(q[0])
+circuit.cx(q[0], q[1])
+circuit.measure(q, b)
+
+qi_job = execute(circuit, backend=qi_backend, shots=256)
+qi_result = qi_job.result()
+histogram = qi_result.get_counts(circuit)
+print('\nState\tCounts')
+[print('{0}\t\t{1}'.format(state, counts)) for state, counts in histogram.items()]
+# Print the full state probabilities histogram
+probabilities_histogram = qi_result.get_probabilities(circuit)
+print('\nState\tProbabilities')
+[print('{0}\t\t{1}'.format(state, val)) for state, val in probabilities_histogram.items()]
